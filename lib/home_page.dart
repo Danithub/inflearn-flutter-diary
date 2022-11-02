ae363e7fb69f8bb6ff041f1122ab58aea9860c3a
DAN <life4789@gmail.com>
2022-11-03 06:29:14 +0900

    reset source

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,9 +2,14 @@ import 'package:diary/diary_service.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Consumer<DiaryService>(
